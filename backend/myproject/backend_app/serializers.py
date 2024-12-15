39314f4d09120ab24f603072ab8edd81a73405d3
nnavidd <navid.nabaeei@gmail.com>
2024-12-15 21:15:30 +0100

    feat: add an 'is_admin' claim inside the token to tag it as an admin user type.
    changes:
    	- add 'CustomTokenObtainPairSerializer' as a serializer to make creating a customized token possible.
    	- change the api/token to use  this new serializer.
    	- add token options in settings.py.
    	- add JWT decoder func in front-end to check the tokens, and alerts in case of admin user login.
    	- optimize the loginView func in views.py according to the new tokenization.

--- backend/myproject/backend_app/serializers.py
+++ backend/myproject/backend_app/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, min_length=8)
@@ -16,3 +17,15 @@ class RegisterSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         # Create the user with a hashed password
         return User.objects.create_user(**validated_data)
+
+# The custom token serializer to make a possibility of adding the desired claim as 'is_admin'
+# to check the user in front-end into recognize the admin type users.
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['is_admin'] = user.is_superuser
+
+        return token
