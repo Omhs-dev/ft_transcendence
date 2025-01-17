c885550ede3adb0b1e89a355245fbc153e75b044
nnavidd <navid.nabaeei@gmail.com>
2025-01-17 22:41:50 +0100

    set the email sending in Django

--- backend/Django_backend_project/settings/settings.py
+++ backend/Django_backend_project/settings/settings.py
@@ -221,6 +221,17 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Replace with your email provider's SMTP server
+EMAIL_PORT = 587  # Port for TLS
+EMAIL_USE_TLS = True  # Use TLS (secure connection)
+EMAIL_HOST_USER = 'transcendence.42institute@gmail.com'  # Your email address
+# EMAIL_HOST_PASSWORD = 'Tr@nscendence@42'  # Your email password or app password
+EMAIL_HOST_PASSWORD = 'tmsw qfws foxe xhbm'  # Your email password or app password
+DEFAULT_FROM_EMAIL = 'Transcendence'
+
+
 STATICFILES_DIRS = [
     STATIC_DIR,
 ]
